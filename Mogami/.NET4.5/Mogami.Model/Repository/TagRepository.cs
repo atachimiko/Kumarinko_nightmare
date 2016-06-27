19e7b501756008e9e51ec1ab547e3525d73d59cc
atachimiko <atachi.miko@gmail.com>
2016-06-27 23:35:27 +0900

    Tagリポジトリの追加

---
+++ Mogami/.NET4.5/Mogami.Model/Repository/TagRepository.cs
@@ -0,0 +1,42 @@
+﻿using Akalib.Entity.Repository;
+using log4net;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mogami.Model.Repository
+{
+	public class TagRepository : GenericRepository<Tag>
+	{
+
+		#region フィールド
+
+		static ILog LOG = LogManager.GetLogger(typeof(TagRepository));
+
+		#endregion フィールド
+
+
+		#region コンストラクタ
+
+		public TagRepository(DbContext context)
+			: base(context)
+		{
+		}
+
+		#endregion コンストラクタ
+
+
+		#region メソッド
+
+		public Tag Load(long id)
+		{
+			return _dbset.Where(x => x.Id == id).FirstOrDefault();
+		}
+
+		#endregion メソッド
+
+	}
+}
