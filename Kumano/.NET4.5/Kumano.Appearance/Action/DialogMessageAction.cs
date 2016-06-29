fb8779e0b94a9c6bec8ae8e6e56d87fc5d940a38
atachimiko <atachi.miko@gmail.com>
2016-06-30 00:25:37 +0900

    ダイアログの表示インフラ

---
+++ Kumano/.NET4.5/Kumano.Appearance/Action/DialogMessageAction.cs
@@ -0,0 +1,34 @@
+﻿using Kumano.Core;
+using Kumano.Data.Infrastructure;
+using Livet.Behaviors.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Kumano.View.Action
+{
+	public class DialogMessageAction : InteractionMessageAction<FrameworkElement>
+	{
+		protected override void InvokeAction(Livet.Messaging.InteractionMessage m)
+		{
+			////このアクションが対応するメッセージに変換します。
+			var confirmMessage = m as DialogMessage;
+			if (confirmMessage == null)
+			{
+				return;
+			}
+
+			Window dialog = null;
+
+			// ダイアログの表示
+			if (dialog != null)
+			{
+				dialog.DataContext = confirmMessage.ViewModel;
+				dialog.Owner = ApplicationContext.MainWindow;
+				dialog.ShowDialog();
+			}
+		}
+}
---
+++ Kumano/.NET4.5/Kumano.Data/ViewModel/EditCategoryDialogViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Akalib.Wpf.Mvvm;
+using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kumano.Data.ViewModel
+{
+	/// <summary>
+	/// EditCategoryDialog
+	/// </summary>
+	public class EditCategoryDialogViewModel : DialogViewModelBase
+	{
+
+
+		#region フィールド
+
+		static ILog LOG = LogManager.GetLogger(typeof(EditCategoryDialogViewModel));
+
+		#endregion フィールド
+
+
+		#region メソッド
+
+		protected override bool OnRequestClose()
+		{
+			return true;
+		}
+
+		#endregion メソッド
+
+	}
+}
