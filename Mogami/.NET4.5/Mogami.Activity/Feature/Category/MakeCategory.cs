5577108b84e9dce6178156f1a6bd85c4dcc14a46
atachimiko <atachi.miko@gmail.com>
2016-07-02 08:40:08 +0900

    ”MakeCategory”アクティビティを追加

---
+++ Mogami/.NET4.5/Mogami.Activity/Feature/Category/MakeCategory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+using log4net;
+
+namespace Mogami.Activity.Feature.Category
+{
+
+	public sealed class MakeCategory : CodeActivity
+	{
+
+		#region フィールド
+
+		static ILog LOG = LogManager.GetLogger(typeof(MakeCategory));
+
+		#endregion フィールド
+
+
+		#region メソッド
+
+		protected override void Execute(CodeActivityContext context)
+		{
+
+		}
+
+		#endregion メソッド
+	}
+}
--- Mogami/.NET4.5/Mogami.Activity/Feature/Content/MakeArtifact.cs
+++ Mogami/.NET4.5/Mogami.Activity/Feature/Content/MakeArtifact.cs
@@ -50,6 +50,23 @@ namespace Mogami.Activity.Feature.Content
 				if (a != null) throw new ApplicationException("すでに作成済みのFileMappingInfoです。");
 			}
 
+			var catrepo = new CategoryRepository(workflowContext.DbContext);
+			var appcat = catrepo.Load(3L);
+			if (appcat == null) throw new ApplicationException();
+
+			Category targetCategory = appcat;
+			var tokens = target.MappingFilePath.Split(new string[] { @"\" }, StringSplitOptions.None);
+			var sttokens = new Stack<string>(tokens);
+			var title = sttokens.Pop();
+			var qutokens = new Queue<string>(sttokens.Reverse<string>());
+			while (qutokens.Count > 0)
+			{
+				var oneText = qutokens.Dequeue();
+				targetCategory = CreateOrSelectCategory(targetCategory, oneText, catrepo);
+			}
+
+			workflowContext.DbContext.SaveChanges();
+
 			// 現Verでは画像のみ、メタ情報を生成できる。
 			// (それ以外のファイルは、例外を投げる)
 			if (target.Mimetype == "image/png")
@@ -57,12 +74,21 @@ namespace Mogami.Activity.Feature.Content
 				var repo = new ImageArtifactRepository(workflowContext.DbContext);
 				var entity = new ImageArtifact
 				{
-					Title = target.MappingFilePath,
+					Title = title,
 					IdentifyKey = RandomAlphameric.RandomAlphanumeric(10),
 					FileMappingInfo = target,
 					ImageHeight = -1, // 未実装
 					ImageWidth = -1, // 未実装
 				};
+
+				if (targetCategory != null)
+					entity.Category = new T_Artifact2Category()
+					{
+						Artifact = entity,
+						Category = targetCategory,
+						OrderNo = 1
+					};
+
 				repo.Add(entity);
 
 				pstack.SetValue(outputname, entity);
@@ -73,6 +99,32 @@ namespace Mogami.Activity.Feature.Content
 			}
 		}
 
+		private Category CreateOrSelectCategory(Category parentCategory, string categoryName, CategoryRepository repo)
+		{
+			Category category = null;
+			foreach (var child in parentCategory.ChildCategories)
+			{
+				if(child.Name == categoryName)
+				{
+					category = child;
+					break;
+				}
+			}
+
+			// Create
+			if (category == null)
+			{
+				category = new Category()
+				{
+					Name = categoryName,
+					CategoryTypeCode = Core.Constructions.CategoryType.APPLICATION,
+					ParentCategory = parentCategory
+				};
+				repo.Add(category);
+			}
+
+			return category;
+		}
 		#endregion メソッド
 	}
 }
